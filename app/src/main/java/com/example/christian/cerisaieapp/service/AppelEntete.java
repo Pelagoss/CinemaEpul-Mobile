df836a3fc4b8e27449b47eed020f6511bf77b1d3
Pelagoss <pelagoss@outlook.fr>
2022-03-14 12:41:01 +0100

    upd

--- app/src/main/java/com/example/christian/cerisaieapp/service/AppelEntete.java
+++
@@ -1,8 +0,0 @@
-package com.example.christian.cerisaieapp.service;
-
-public class AppelEntete {
-
-   public static  final String ENDPOINT = "http://192.168.17.100:8080/";
-   // public static  final String ENDPOINT = "http://134.214.116.81:8080/";
-
-}
---
+++ app/src/main/java/com/example/cinepul/cinepulapp/service/AppelEntete.java
@@ -0,0 +1,8 @@
+package com.example.cinepul.cinepulapp.service;
+
+public class AppelEntete {
+
+   public static  final String ENDPOINT = "http://10.42.131.71:5000/";
+   // public static  final String ENDPOINT = "http://134.214.116.81:8080/";
+
+}
